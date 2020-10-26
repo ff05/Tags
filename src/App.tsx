9bca932d9f27f077d897e3828e4c74cba36c1b87
Frank <frankfilius@saxum.nl>
2020-10-26 09:28:22 +0100

    Use the router to render pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ListOverviewPage from "./pages/ListOverview/ListOverview";
+import SingleListPage from "./pages/SingleList/SingleList";
+import TagsProvider from "./providers/TagsProvider";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <TagsProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ListOverviewPage />
+          </Route>
+          <Route exact path="/:listName">
+            <SingleListPage />
+          </Route>
+        </Switch>
+      </Router>
+    </TagsProvider>
+  );
 }
 
 export default App;
