9b6c8a2efe4bea60d9537cef8a8360381b53bb30
Frank <frankfilius@saxum.nl>
2020-10-30 16:52:02 +0100

    Use id instead of name as route param

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <Route exact path="/">
             <ListOverviewPage />
           </Route>
-          <Route exact path="/:listName">
+          <Route exact path="/:id">
             <SingleListPage />
           </Route>
         </Switch>
