2cb60a330549ca217111ae0777f261494bfc3f4b
Frank <frankfilius@saxum.nl>
2020-10-30 16:57:17 +0100

    Update the provider with all necessary methods

--- src/providers/TagsProvider.tsx
+++ src/providers/TagsProvider.tsx
@@ -1,6 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { change, remove, fetch, add } from "../utils/api";
 
-interface ITagsList {
+const URL = "http://localhost:3004/tagLists";
+
+export interface ITagsList {
   id: number;
   name: string;
   tags: string[];
@@ -9,31 +12,67 @@ interface ITagsList {
 interface IContextProps {
   tagsLists: ITagsList[];
   addList: (name: string) => void;
-  removeList: (name: string) => void;
+  removeList: (id: number) => void;
+  updateTagList: (tagName: string, listId: number) => void;
 }
 
 export const TagsContext = createContext<IContextProps>({
   tagsLists: [],
   addList: () => {},
   removeList: () => {},
+  updateTagList: () => {},
 });
 
 const TagsProvider: React.FC = ({ children }) => {
   const [tagsLists, setTagsLists] = useState<ITagsList[]>([]);
+  const [dbUpdated, setDbUpdated] = useState(false);
+
+  useEffect(() => {
+    fetch<ITagsList[]>(URL)
+      .then((data) => setTagsLists(data))
+      .catch((error) => console.error(error));
+  }, [dbUpdated]);
 
   const addList = (name: string) => {
-    setTagsLists((tagsLists) => tagsLists.concat({ [name]: [] }));
+    add(URL, { name, tags: [] })
+      .then(() => setDbUpdated(!dbUpdated))
+      .catch((error) => console.error(error));
   };
 
-  //   addTagToList = (name: string) => {
-  //       const tag = tagsLists.find()
-  //   }
-
-  const removeList = (name: string) => {
-    setTagsLists((tagsLists) => tagsLists.filter((list) => name !== Object.keys(list)[0]));
+  const removeList = (id: number) => {
+    remove(URL + "/" + id)
+      .then(() => {
+        setDbUpdated(!dbUpdated);
+      })
+      .catch((error) => console.error(error));
   };
 
-  return <TagsContext.Provider value={{ tagsLists, addList, removeList }}>{children}</TagsContext.Provider>;
+  const updateTagList: (tagName: string, listId: number) => void = (tagName: string, listId: number) => {
+    const currentList = tagsLists.find((list) => list.id === listId);
+    if (!currentList) {
+      return;
+    }
+    let newList: ITagsList;
+    if (currentList.tags.includes(tagName)) {
+      const [tagName, ...otherTags] = currentList.tags;
+      newList = {
+        ...currentList,
+        tags: [...otherTags],
+      };
+    } else {
+      newList = {
+        ...currentList,
+        tags: [...currentList.tags, tagName],
+      };
+    }
+    change(URL + "/" + listId, { ...newList })
+      .then(() => setDbUpdated(!dbUpdated))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <TagsContext.Provider value={{ addList, updateTagList, tagsLists, removeList }}>{children}</TagsContext.Provider>
+  );
 };
 
 export default TagsProvider;
