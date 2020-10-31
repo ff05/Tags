1e5441d9e4288868b88bd8b1eef34068a575cbd5
Frank <frankfilius@saxum.nl>
2020-10-31 15:41:50 +0100

    Create and use page wrapper styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ListOverviewPage from "./containers/ListOverview/ListOverview";
 import SingleListPage from "./containers/SingleList/SingleList";
 import TagsProvider from "./providers/TagsProvider";
+import PageWrapperStyles from "./containers/shared/PageWrapperStyles";
 
 function App() {
   return (
     <TagsProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <ListOverviewPage />
-          </Route>
-          <Route exact path="/:id">
-            <SingleListPage />
-          </Route>
-        </Switch>
-      </Router>
+      <PageWrapperStyles>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <ListOverviewPage />
+            </Route>
+            <Route exact path="/:id">
+              <SingleListPage />
+            </Route>
+          </Switch>
+        </Router>
+      </PageWrapperStyles>
     </TagsProvider>
   );
 }
---
+++ src/containers/shared/PageWrapperStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const PageWrapperStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-gap: 24px;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export default PageWrapperStyles;
