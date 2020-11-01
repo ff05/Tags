9e5f178f2a53f2d07298ac1bc187ccffc5e295f4
Frank <frankfilius@saxum.nl>
2020-11-01 09:53:51 +0100

    Add side-padding to small screen sizes

--- src/containers/shared/PageWrapperStyles.ts
+++ src/containers/shared/PageWrapperStyles.ts
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 const PageWrapperStyles = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-gap: 24px;
-  max-width: 1200px;
-  margin: 0 auto;
+  padding: 0 16px;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 24px;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0;
+  }
 `;
 
 export default PageWrapperStyles;
