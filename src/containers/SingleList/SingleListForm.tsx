2f9ce38598e9a2866b9e562920f763d73313b10a
Frank <frankfilius@saxum.nl>
2020-11-01 20:19:04 +0100

    Create component for the form in single-list component

---
+++ src/containers/SingleList/SingleListForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { ErrorStyles, FormStyles } from "./SingleListForm.styles";
+
+type Props = { validate: (value: string) => boolean; onSubmit: (value: string) => void };
+
+const SingleListForm: React.FC<Props> = ({ validate, onSubmit }) => {
+  const [value, setValue] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setError("");
+    setValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (validate(value)) {
+      onSubmit(value);
+      setValue("");
+    } else {
+      setError("Deze tag bestaat al.");
+    }
+  };
+
+  return (
+    <FormStyles onSubmit={handleSubmit}>
+      <label htmlFor="list-name">Naam</label>
+      <div>
+        <input id="list-name" type="text" value={value} onChange={handleChange} required />
+        <input type="submit" value="Maak aan" />
+      </div>
+      {error && <ErrorStyles>{error}</ErrorStyles>}
+    </FormStyles>
+  );
+};
+
+export default SingleListForm;
