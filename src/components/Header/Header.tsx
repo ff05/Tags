daab1e0d7f250eeedc91ec738d8338f129c72df9
Frank <frankfilius@saxum.nl>
2020-11-02 14:32:09 +0100

    Remove unused import

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { HeaderStyles } from "./Header.styles";
-import { LogoShort } from "@amsterdam/asc-assets";
 import HeaderTitle, { TitleProps } from "./HeaderTitle";
 
 const Header: React.FC<TitleProps> = ({ title }) => {
